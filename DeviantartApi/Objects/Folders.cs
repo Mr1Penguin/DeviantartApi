f826a108e3a7f95580cd80ea5cc840e1dc7218ca
Igor Sharapov <igorsharapov@outlook.com>
2016-08-20 00:01:07 +0300

    collections/folders request

---
+++ DeviantartApi/Objects/Folders.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeviantartApi.Objects
+{
+    public class Folders : Pageable
+    {
+        [JsonProperty("results")]
+        public List<FolderClass> Results { get; set; }
+
+        public class FolderClass
+        {
+            [JsonProperty("folderid")]
+            public string FolderId { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("size")]
+            public int Size { get; set; }
+            [JsonProperty("deviations")]
+            public List<Deviation> Deviation { get; set; }
+        }
+    }
+}
---
+++ DeviantartApi/Requests/Collections/FoldersRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviantartApi.Requests.Collections
+{
+    public class FoldersRequest : PageableRequest<Objects.Folders>
+    {
+        public bool LoadMature { get; set; }
+        public string UserName { get; set; }       
+        public bool CalculateSize { get; set; }
+        public bool ExtPreload { get; set; }
+
+        public override async Task<Response<Objects.Folders>> ExecuteAsync()
+        {
+            return await ExecuteDefaultGetAsync("collections/folders?" +
+                                                $"username={UserName}" +
+                                                (Offset != null ? $"&offset={Offset}" : "") +
+                                                (Limit != null ? $"&limit={Limit}" : "") +
+                                                $"&calculate_size={CalculateSize.ToString().ToLower()}" +
+                                                $"&ext_preload={ExtPreload.ToString().ToLower()}" +
+                                                $"&mature_content={LoadMature.ToString().ToLower()}");
+        }
+    }
+}
