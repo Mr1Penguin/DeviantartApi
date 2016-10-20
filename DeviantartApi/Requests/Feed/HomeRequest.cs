4514faefce67fc388873707faac2881594c2cd85
Igor Sharapov <igorsharapov@outlook.com>
2016-10-20 23:24:54 +0300

    feed/home documentation

--- DeviantartApi/Requests/Feed/HomeRequest.cs
+++ DeviantartApi/Requests/Feed/HomeRequest.cs
@@ -4,8 +4,16 @@ using System.Threading.Tasks;
 
 namespace DeviantartApi.Requests.Feed
 {
+    /// <summary>
+    /// Fetch Watch Feed 
+    /// <para>GET https://www.deviantart.com/api/v1/oauth2/feed/home</para>
+    /// <para>Required scopes: feed</para>
+    /// </summary>
     public class HomeRequest : PageableRequest<Objects.ArrayOfItems<Objects.SubObjects.FeedItem>>
     {
+        /// <summary>
+        /// Include mature content?
+        /// </summary>
         [Parameter("mature_content")]
         public bool MatureContent { get; set; }
 
