90be8db156dc1a40b2d0917a6a5420008a12d02c
Igor Sharapov <igorsharapov@outlook.com>
2016-11-02 00:43:59 +0300

    placebo documentation

--- DeviantartApi/Requests/Utils/PlaceboRequest.cs
+++ DeviantartApi/Requests/Utils/PlaceboRequest.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace DeviantartApi.Requests
 {
+    /// <summary>
+    /// Placebo call to confirm your access_token is valid 
+    /// </summary>
     public class PlaceboRequest : Request<PlaceboStatus>
     {
         public override async Task<Response<PlaceboStatus>> ExecuteAsync()
