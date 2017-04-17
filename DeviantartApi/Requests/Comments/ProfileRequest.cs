4dfcc21b54002713a071c121ae7133d1e63eada8
Igor Sharapov <igorsharapov@outlook.com>
2017-04-17 16:02:25 +0300

    fix namespace of request

--- DeviantartApi/Requests/Comments/ProfileRequest.cs
+++ DeviantartApi/Requests/Comments/ProfileRequest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace DeviantartApi.Requests.Comments.Profile
+namespace DeviantartApi.Requests.Comments
 {
     using System.Threading;
 
