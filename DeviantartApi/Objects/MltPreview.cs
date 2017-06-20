5ff0665589eb7a141c9d2c408ccc7e20f980bf5e
Igor Sharapov <igorsharapov@outlook.com>
2017-06-20 15:32:59 +0300

    MLT preview request

---
+++ DeviantartApi/Objects/MltPreview.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DeviantartApi.Objects
+{
+    public class MltPreview : BaseObject
+    {
+        [JsonProperty("seed")]
+        public string Seed { get; set; }
+
+        [JsonProperty("Author")]
+        public User Author { get; set; }
+
+        [JsonProperty("more_from_artist")]
+        public List<Deviation> MoreFromArtist { get; set; }
+
+        [JsonProperty("more_from_da")]
+        public List<Deviation> MoreFromDa { get; set; }
+    }
+}
