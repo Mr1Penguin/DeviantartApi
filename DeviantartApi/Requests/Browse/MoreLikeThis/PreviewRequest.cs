f4c8eedcabefb705c1faa8d16fed3e58178fab8e
Igor Sharapov <igorsharapov@outlook.com>
2017-08-03 15:17:30 +0300

    test full documentation

--- DeviantartApi/Requests/Browse/MoreLikeThis/PreviewRequest.cs
+++ DeviantartApi/Requests/Browse/MoreLikeThis/PreviewRequest.cs
@@ -6,20 +6,39 @@ using System.Threading.Tasks;
 namespace DeviantartApi.Requests.Browse.MoreLikeThis
 {
     /// <summary>
-    /// Fetch More Like This preview result for a seed deviation 
+    /// Fetch More Like This preview result for a seed deviation.
     /// </summary>
     public class PreviewRequest : Request<Objects.MltPreview>
     {
+        /// <summary>
+        /// Request's specific error list.
+        /// </summary>
         public enum ErrorCode
         {
+            /// <summary>
+            /// Invalid Seed Requested.
+            /// </summary>
             InvalidSeedRequested = 0
         }
 
+        /// <summary>
+        /// Expansions for request.
+        /// </summary>
+        /// <remarks>
+        /// To specify multiple expansion options, separate each option with a comma.
+        /// Note: Using expansion options counts as 1 extra request per option toward your clients rate limit.
+        /// </remarks>
         public enum UserExpand
         {
+            /// <summary>
+            /// Add <c>IsWatching</c> flag to author in response.
+            /// </summary>
             Watch
         }
 
+        /// <summary>
+        /// Expansions for <see cref="Objects.User"/> object.
+        /// </summary>
         [Parameter("user")]
         [Expands]
         public HashSet<UserExpand> UserExpands { get; set; } = new HashSet<UserExpand>();
@@ -30,9 +49,20 @@ namespace DeviantartApi.Requests.Browse.MoreLikeThis
         [Parameter("seed")]
         public string Seed { get; set; }
 
+        /// <summary>
+        /// Get response with mature content.
+        /// </summary>
         [Parameter("mature_content")]
         public bool MatureContent { get; set; }
 
+        /// <summary>
+        /// Execute request async.
+        /// </summary>
+        /// <param name="cancellationToken">Token for cancellation of request.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains <see cref="Objects.MltPreview"/> wrapped in <see cref="Response{T}"/> struct.
+        /// </returns>
         public override Task<Response<Objects.MltPreview>> ExecuteAsync(CancellationToken cancellationToken)
         {
             var values = new Dictionary<string, string>();
