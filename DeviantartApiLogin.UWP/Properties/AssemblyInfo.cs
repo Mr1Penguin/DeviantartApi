85e4f118ed11fc53192df873bff1c49290e01bdd
Igor Sharapov <igorsharapov@outlook.com>
2017-08-16 15:21:54 +0300

    update lib reference and add some meta info

--- DeviantartApiLogin.UWP/Properties/AssemblyInfo.cs
+++ DeviantartApiLogin.UWP/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DeviantartApiLogin.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Login process for UWP")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DeviantartApiLogin.UWP")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: ComVisible(false)]
